16862208610709a7d1910e67c2dce1d0bfb2ded9
Renata Phamova <49619564+phamobic@users.noreply.github.com>
2022-12-01 12:39:44 +0100

    challenge: Day 01 (#1)

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun insertCaloriesToSortedMax(topMaxCalories: MutableList<Long>, calories: Long) {
+        val index = topMaxCalories.indexOfFirst { calories > it }
+
+        if (index < 0) return
+
+        topMaxCalories.add(index, calories)
+        topMaxCalories.removeLast()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun getMaxCaloriesSum(numberOfElves: Int, caloriesInput: List<String>): Long {
+        val topMaxCalories = MutableList(numberOfElves) { 0L }
+        var currentElfCalories = 0L
+
+        caloriesInput.forEach { line ->
+            if (line.isBlank()) {
+                insertCaloriesToSortedMax(topMaxCalories, currentElfCalories)
+                currentElfCalories = 0L
+            } else {
+                currentElfCalories += line.toLong()
+            }
+        }
+        insertCaloriesToSortedMax(topMaxCalories, currentElfCalories)
+
+        return topMaxCalories.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
+    fun part1(input: List<String>): Long = getMaxCaloriesSum(1, input)
+
+    fun part2(input: List<String>): Long = getMaxCaloriesSum(3, input)
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    check(part1(testInput) == 24000L)
+    check(part2(testInput) == 45000L)
 }
